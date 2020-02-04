53c6f278d6f8edd5c08a05c5e16dc505eb3c4ec1
Captain-G <poppinpites7@gmail.com>
2020-02-04 11:41:29 +0300

    Introduction to PHP

---
+++ lesson1.php
@@ -0,0 +1,32 @@
+<?php
+//Introduction to Php
+//Php is a widely used open source scripting laguage
+//php scripts are executed on the server
+//php files can contain text,html,css,javascript and php code
+//php code is executed on the server and the result is returned to the browser as plain html
+//php files have the extension ".php"
+
+echo "Hello World";
+echo "<h1>Hello World</h1>";
+//echo; print content in the browser
+//hello world is the content to be printed in the browser
+//; means end of php statement or instruction
+
+//VARIABLES IN PHP
+//a variable is a container for storing data
+$myname="Peter Gachuki";
+//a variable must start with a dollar sign
+echo $myname;
+$company1="Safaricom <br>";
+$company2="Oppo <br>";
+$company3="Windows <br>";
+
+echo "<br>";
+
+echo $company1;
+echo $company2;
+echo $company3;
+
+?>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consectetur ea exercitationem nihil possimus quidem ratione, suscipit. Atque, ducimus eum eveniet exercitationem labore minus nihil nostrum perspiciatis, quo, repellat ut.</p>
+
